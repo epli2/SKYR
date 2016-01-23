a15f2826c98b67ed268c742ab57c558c2c98a9ca
epli2 <motoryo1@gmail.com>
2016-01-23 13:15:57 +0900

    add Triangulation_v_ostream_2.hpp

---
+++ Triangulation_v_ostream_2.hpp
@@ -0,0 +1,32 @@
+#include <CGAL/Triangulation_2.h>
+#include <iostream>
+#include <sstream>
+
+namespace CGAL {
+
+  template < class GT, class TDS >
+  std::string
+  my_show_triangulation_faces(const Triangulation_2<GT,TDS> &T)
+  {
+    std::ostringstream os;
+    // Finite vertices coordinates.
+    std::map<typename Triangulation_2<GT, TDS>::Vertex_handle, int> V;
+    int inum = 0;
+    for( typename Triangulation_2<GT, TDS>::Vertex_iterator
+      vit = T.vertices_begin(); vit != T.vertices_end(); ++vit) {
+        V[vit] = inum++;
+        // std::cout << vit->point() << std::endl;
+    }
+
+    // Finite faces indices.
+    for( typename Triangulation_2<GT, TDS>::Face_iterator
+      fit = T.faces_begin(); fit != T.faces_end(); ++fit) {
+        for (int i=0; i<3; i++)
+          os << V[fit->vertex(i)] << ",";
+        os << "-1,\n";
+    }
+    return os.str();
+  }
+
+  
+} //namespace CGAL
